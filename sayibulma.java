5a01915b3634f63296857d83fe00997a8d5e07a2
SerenEsen <74063743+SerenEsen@users.noreply.github.com>
2021-05-01 00:39:20 +0300

    Add files via upload

--- sayibulma.java
+++ sayibulma.java
@@ -3,8 +3,8 @@ package dizilerdearama;
 public class sayibulma {
 
 	public static void main(String[] args) {
-		int[] sayilar = new int[] { 1, 2, 5, 7, 9, 0 };
-		int aranacak = 6;
+		int[] sayilar = new int[] { 1, 5, 8, 7,3, 2 };
+		int aranacak = 5;
 		boolean varMi = false;
 
 		for (int sayi : sayilar) {
