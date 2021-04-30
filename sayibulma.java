a4acebde64c1e0dc241e0fc9fe24cae87514c161
SerenEsen <74063743+SerenEsen@users.noreply.github.com>
2021-04-30 22:28:27 +0300

    Add files via upload

---
+++ sayibulma.java
@@ -0,0 +1,23 @@
+package dizilerdearama;
+
+public class sayibulma {
+
+	public static void main(String[] args) {
+		int[] sayilar = new int[] { 1, 2, 5, 7, 9, 0 };
+		int aranacak = 6;
+		boolean varMi = false;
+
+		for (int sayi : sayilar) {
+			if (sayi == aranacak) {
+				varMi = true;
+				break;
+			}
+		}
+		if (varMi) {
+			System.out.println("Aranan sayý bulundu.");
+		} else {
+			System.out.println("Aranan sayý bulunamadý.");
+		}
+	}
+
+}
