66d507a7b5f3aae738fd689b00205897079630b9
SerenEsen <74063743+SerenEsen@users.noreply.github.com>
2021-05-03 02:29:38 +0300

    lesson2_homework
    odev

---
+++ lesson2_homework/StudentManager.java
@@ -0,0 +1,11 @@
+package lesson2_homework;
+
+public class StudentManager {
+	public void addcourse(Course courses) {
+		System.out.println(courses.coursename +" added your course list!" );
+	}
+	public void deletecourse(Course courses) {
+		System.out.println(courses.coursename +" deleted the course!" );
+	}
+
+}
