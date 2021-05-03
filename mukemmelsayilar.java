01116f401f83752a7a3514234f5feb20e3857746
SerenEsen <74063743+SerenEsen@users.noreply.github.com>
2021-05-03 13:44:35 +0300

    javapracties
    mukemmelsayilar

--- mukemmelsayilar.java
+++ mukemmelsayilar.java
@@ -1,22 +1,24 @@
-package mukemmelsayilar;
+package primenumber;
 
 public class mukemmelsayilar {
 
-	public static void main(String[] args) {
-				
-		int number = 30;
-		int total = 0;
-		for(int i=1; i<number; i++) {
-			if(number % i == 0) {
-				total+=i;}
-		}
-		if(total==number) {
-			System.out.println("Mükemmel bir sayýdýr");
-		}
-		else {
-			System.out.println("Mükemmel bir sayý deðildir");
-		}
-	}	
+
+			public static void main(String[] args) {
+			
+				int number = 30;
+				int total = 0;
+				for(int i=1; i<number; i++) {
+					if(number % i == 0) {
+						total+=i;				
+					}
+				}
+				if(total==number) {
+					System.out.println("Mükemmel bir sayýdýr");
+				}else {
+					System.out.println("Mükemmel bir sayý deðildir");
+				}
+			}	
 
 }
+
 
